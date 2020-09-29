245547840b15162b528882de668f85acca757a8f
Hashir Sheikh <hashir.s@ogenie.app>
2020-09-29 18:30:11 +0530

    send genric template

--- templates/sendGenericTemplate.js
+++ templates/sendGenericTemplate.js
@@ -7,21 +7,22 @@ module.exports = function sendGenericTemplate(recipientId, respBody) {
    console.log("-------------------15------------------");
   // for (let i = 0; i < respBody.length; i++) { // I dont like using forEach
 
-      // let obj = {
-      //        "title":"Test Title",
-      //        "image_url": "https://cdn130.picsart.com/316113773147201.png?type=webp&to=min&r=640",
-      //        "subtitle": 'Subtitle',
-      //      }
-      //
-      // let messageData = {
-      //    "attachment": {
-      //    "type": "template",
-      //    "payload": {
-      //          "template_type": "generic",
-      //          "elements": [obj,obj]
-      //       }
-      //    }
-      // }
+      let obj = {
+             "title":"Test Title",
+             "image_url": "https://cdn130.picsart.com/316113773147201.png?type=webp&to=min&r=640",
+             "subtitle": 'Subtitle',
+           }
+
+      let messageData = {
+         // "attachment": {
+         // "type": "template",
+         // "payload": {
+         //       "template_type": "generic",
+         //       "elements": [obj,obj]
+         //    }
+         // }
+         "text":ans
+      }
 
       request({
        url: 'https://graph.facebook.com/v8.0/me/messages',
@@ -29,7 +30,7 @@ module.exports = function sendGenericTemplate(recipientId, respBody) {
        method: 'POST',
        json: {
          recipient: {id: recipientId},
-         message: ans
+         message: messageData
       }
     }, function(error, response, body){
       console.log(body);
