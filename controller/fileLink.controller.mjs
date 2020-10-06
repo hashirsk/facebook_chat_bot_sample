90806351db1aeafe04c447b73a920665c5231342
hashir <hashir.s@ogenie.app>
2020-10-07 01:39:30 +0530

    s

--- controller/fileLink.controller.mjs
+++ controller/fileLink.controller.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 
 export const createFileLink = (req, res) => {
     // 
+    console.log('path is ==>', dirname(fileURLToPath(import.meta.url)))
     const file = `${dirname(fileURLToPath(import.meta.url))}/../attachment/${req.params.userid}/${req.params.ext}/${req.params.filename}`;
     console.log("got it", file);
     res.download(file);
